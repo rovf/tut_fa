428376fd31e30074555d8e4e0d0c05c2007a0ea1
rovf <ynnor@mm.st>
2014-06-22 11:46:05 +0200

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def new
+     @user=User.new
   end
   def show
      @user=User.find(params[:id])
+  end
+  def create
+     # The following ("parameter mass assignment", because params[:user]
+     # is a hash) worked in Rails 3, but it is forbidden in Rails 4
+     # ("ActiveModel::ForbiddenAttributesError") for security reason.
+     @user=User.new(user_params)
+     if @user.save
+        flash[:success]='You are in!'
+         redirect_to @user
+     else
+         render 'new'
+     end
+  end
+
+  private
+
+  def user_params
+     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
